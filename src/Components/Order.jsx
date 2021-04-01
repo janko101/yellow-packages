4d464218304a0515edc3f41704aa081d98eb3c47
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-04-01 12:33:29 +0200

    Adds background image and styles order card

--- src/Components/Order.jsx
+++ src/Components/Order.jsx
@@ -2,26 +2,27 @@ import React from "react";
 
 export default function Order({ orderData }) {
   const formattedDeliveryDate = new Intl.DateTimeFormat("en-GB", {
-    dateStyle: "full",
+    dateStyle: "short",
     timeStyle: "short",
   }).format(new Date(orderData.eta));
 
   const lastUpdatedTime = new Intl.DateTimeFormat("en-GB", {
-    timeStyle: "long",
+    timeStyle: "short",
   }).format(new Date(orderData.last_updated));
 
   return (
     <article className="order-info">
-      <p>Package ID: {orderData.parcel_id}</p>
-      <p>Status: {orderData.status}</p>
-      <p>
-        Estimated Delivery Time:{" "}
-        {orderData.status === "delivered" ? "delivered" : formattedDeliveryDate}
-      </p>
-      <p>Sender: {orderData.sender}</p>
-      <p>Pickup Location: {orderData.location_name} </p>
-      <p>Last Updated: {lastUpdatedTime}</p>
-      <hr />
+      <p>{`Package ID: ${orderData.parcel_id}`}</p>
+      <p>{`Sender: ${orderData.sender}`}</p>
+      <p>{`Status: ${orderData.status}`}</p>
+
+      {orderData.status !== "ready-for-pickup" &&
+        orderData.status !== "delivered" && (
+          <p>{`ETA: ${formattedDeliveryDate}h`}</p>
+        )}
+
+      <p>{`Pickup Location: ${orderData.location_name}`}</p>
+      <p>{`Last Updated: ${lastUpdatedTime}h`}</p>
     </article>
   );
 }
