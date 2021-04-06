1bb0e30a6ad786d60c569659802f73acf82203a0
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-04-06 16:11:57 +0200

    Formats delivery status

--- src/Components/Order.jsx
+++ src/Components/Order.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Order({ orderData }) {
+  //Constants
   const formattedDeliveryDate = new Intl.DateTimeFormat("en-GB", {
     dateStyle: "short",
     timeStyle: "short",
@@ -10,6 +11,22 @@ export default function Order({ orderData }) {
     timeStyle: "short",
   }).format(new Date(orderData.last_updated));
 
+  // Methods
+  function formattedStatus(status) {
+    switch (status) {
+      case "delivered":
+        return "Delivered";
+      case "on-the-way":
+        return "On The Way";
+      case "order-info-received":
+        return "Order Info Received";
+      case "ready-for-pickup":
+        return "Ready For Pickup";
+      default:
+        return status;
+    }
+  }
+
   return (
     <article className="order-info">
       <div id="first-order-info">
@@ -18,7 +35,7 @@ export default function Order({ orderData }) {
           className="left-order-info"
         >{`Package ID: ${orderData.parcel_id}`}</span>
         <span id="status-info" className="right-order-info">
-          {orderData.status}
+          {formattedStatus(orderData.status)}
         </span>
       </div>
 
